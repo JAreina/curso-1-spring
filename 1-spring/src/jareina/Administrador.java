a5aa1cb44ebd18da0f6a4e20657c440482286d19
JAreina <jareinafdez@gmail.com>
2018-04-06 09:24:55 +0200

    contructor getter setter

--- 1-spring/src/jareina/Administrador.java
+++ 1-spring/src/jareina/Administrador.java
@@ -2,6 +2,44 @@ package jareina;
 
 public class Administrador {
 
+	
+	private int id;
+	private String nombre;
+	
+	
+	public Administrador() {};
+	
+	public Administrador(int id, String nombre) {
+		
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Administrador [id=" + id + ", nombre=" + nombre + "]";
+	}
+
+
+
 	public void imprimir() {
 		System.out.println("hola");
 		
--- 1-spring/src/jareina/Main.java
+++ 1-spring/src/jareina/Main.java
@@ -5,11 +5,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
+	
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring_config.xml");
 
+		
+		
         Administrador admi = (Administrador) applicationContext.getBean("admin");
-        admi.imprimir();
+      //  admi.imprimir();
+        
+        System.out.println(admi);
         ((ClassPathXmlApplicationContext)applicationContext).close();
 	}
 }
